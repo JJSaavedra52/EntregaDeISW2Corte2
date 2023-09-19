566a6034e8aa987039da0159daa93fbee1665d72
Wayer52 <juanjsaavedra52@gmail.com>
2023-09-18 20:07:21 -0500

    Borre código comentado y puse unas validaciones básicas para que no se caiga el programa

--- index.mjs
+++ index.mjs
@@ -4,60 +4,13 @@ import { startConnection } from "./src/mongo/index.mjs";
 import filtro from "./src/handlers/filters/index.mjs"
 import Boom from "@hapi/boom";
 import { PORT } from "./src/commons/env.mjs";
-import multer from "multer";
-// import applyFiltersHandler from "./src/handlers/filters/applyFiltersHandler.mjs";
-import path from "path"
-import ProcessModel from "./src/models/Process.mjs";
 import uploadFiles from "./src/controllers/files/uploadFiles.mjs";
 
 const app = Express();
 app.use(bodyParser.json())
 
-//--------------Multer config----------------------
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads'); // Carpeta donde se guardarán las imágenes
-//     },
-//     filename: function (req, file, cb) {
-//         console.log(file);
-//         cb(null, Date.now() + path.extname(file.originalname));
-//     },
-// });
-
-// const upload = multer({
-//     storage: storage
-// }).array('files', 5);
-// //--------------END Multer config-----------------------------
-
-// //------------New Part that allows images uploads--------------
-// //------------------Image upload-------------------------------
-// app.post('/upload', (req, res) => {
-//     upload(req, res, async (err) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             const files = req.files; // Array de archivos
-//             const filters = req.body.filters;
-
-//             for (const file of files) {
-//                 const newProcess = new ProcessModel({
-//                     files: {
-//                         data: file.filename,
-//                         contentType: file.mimetype
-//                     },
-//                     filters: filters,
-//                 });
-
-//                 await newProcess.save();
-//             }
-
-//             res.send("Se subieron exitosamente " + files.length + " imágenes.");
-//         }
-//     });
-// });
-//--------------------END of image upload--------------------------
+// Ruta que permite subir varias imagenes (solo se puede con un filtro de momento)
 app.post("/upload", uploadFiles);
-
 
 app.use("/image", filtro);
 
--- src/controllers/files/uploadFiles.mjs
+++ src/controllers/files/uploadFiles.mjs
@@ -3,7 +3,6 @@ import multer from "multer";
 import ProcessModel from "../../models/Process.mjs";
 import { PayloadValidation } from "../filters/applyFilters.mjs";
 
-
 //--------------Multer config----------------------
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -21,27 +20,27 @@ const upload = multer({
 //--------------END Multer config-----------------------------
 
 //------------New Part that allows images uploads--------------
-const uploadFiles = async (req, res) => {
-    try {
-        // Validar que el campo 'filters' contenga al menos un filtro válido
-        const filters = req.body.filters;
-        const { error } = PayloadValidation.validate({ filters });
 
-        if (error) {
-            console.error('Error de validación:', error.details);
-            return res.status(422).json({ error: 'Se requiere al menos un filtro válido en el campo "filters".' });
+const uploadFiles = (req, res) => {
+    // Validación
+    const filters = req.body.filters;
+    const { error } = PayloadValidation.validate({ filters });
+
+    if (error) {
+        console.error('Error de validación:', error.details);
+        return res.status(422).json({ error: 'Se requiere al menos un filtro válido en el campo "filters".' });
+    }
+
+    // Continuar con la subida de archivos
+    upload(req, res, async (err) => {
+        if (err) {
+            console.error('Error al subir archivos:', err);
+            return res.status(500).json({ error: 'Error al subir archivos' });
         }
 
-        // Continuar con la subida de archivos
-        upload(req, res, async (err) => {
-            if (err) {
-                console.error('Error al subir archivos:', err);
-                return res.status(500).json({ error: 'Error al subir archivos' });
-            }
-
+        try {
             const files = req.files; // Array de archivos
             const filters = req.body.filters;
-
             for (const file of files) {
                 const newProcess = new ProcessModel({
                     files: {
@@ -51,59 +50,15 @@ const uploadFiles = async (req, res) => {
                     filters: filters,
                 });
 
-                // Intentar guardar el proceso en la base de datos
-                try {
-                    await newProcess.save();
-                } catch (saveError) {
-                    console.error('Error al guardar en la base de datos:', saveError);
-                    // Puedes manejar el error aquí y enviar una respuesta adecuada
-                    return res.status(500).json({ error: 'Error al guardar en la base de datos' });
-                }
+                await newProcess.save();
             }
-
             res.send("Se subieron exitosamente " + files.length + " imágenes.");
-        });
-    } catch (validationError) {
-        console.error('Error de validación:', validationError);
-        return res.status(422).json({ error: 'Error de validación' });
-    }
+        } catch (error) {
+            console.error('Error al guardar en la base de datos:', error);
+            return res.status(500).json({ error: 'Error al guardar en la base de datos' });
+        }
+    });
 };
-
-
-// const uploadFiles = (req, res) => {
-//     // Validar que el campo 'filters' contenga al menos un filtro válido
-//     const filters = req.body.filters;
-//     const { error } = PayloadValidation.validate({ filters });
-
-//     if (error) {
-//         return res.status(422).json({ error: 'Se requiere al menos un filtro válido en el campo "filters".' });
-//     }
-
-//     // Continuar con la subida de archivos
-//     upload(req, res, async (err) => {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).json({ error: 'Error al subir archivos' });
-//         } else {
-//             const files = req.files; // Array de archivos
-//             const filters = req.body.filters;
-
-//             for (const file of files) {
-//                 const newProcess = new ProcessModel({
-//                     files: {
-//                         data: file.filename,
-//                         contentType: file.mimetype
-//                     },
-//                     filters: filters,
-//                 });
-
-//                 await newProcess.save();
-//             }
-
-//             res.send("Se subieron exitosamente " + files.length + " imágenes.");
-//         }
-//     });
-// };
 //--------------------END of image upload--------------------------
 
 export default uploadFiles;
